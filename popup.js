5bf5b7ba1d1b3d761e3db2f63b8d417c468313dc
Kouji Anzai <kanzmrsw@HPE-590jp.(none)>
2013-03-06 14:08:25 +0900

    add css

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-var url = 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=034fe3f2dd3b01bb7eea0a4ccf9fa27c&photoset_id=72157627697709381&extras=url_s&format=rest';
+var url = 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=034fe3f2dd3b01bb7eea0a4ccf9fa27c&photoset_id=72157627697709381&&extras=date_taken%2Curl_s&format=rest';
 xhr.open('GET', url, true);
 xhr.send(null);
 var photo;
@@ -8,7 +8,9 @@ xhr.onreadystatechange = function() {
 	if (xhr.readyState == 4 && xhr.status == 200) {
 		var rsp = xhr.responseXML;
 		photo = rsp.getElementsByTagName('photo');
-		var src = photo[photo.length - 1].attributes[6].value;
+		var src = photo[photo.length - 1].attributes[8].value;
+		var date = photo[photo.length - 1].attributes[6].value.split(' ')[0];
 		document.getElementById('l_img').src = src;
+		document.getElementById('d_str').textContent = date;
 	}
 }
