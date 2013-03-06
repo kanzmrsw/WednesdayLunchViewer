993c212882ed90dc50eb489ed3f4ba22882b84a1
Kouji Anzai <anzai.k.aa@gmail.com>
2013-03-06 16:21:38 +0900

    many changes

--- popup.js
+++ popup.js
@@ -3,14 +3,51 @@ var url = 'http://api.flickr.com/services/rest/?method=flickr.photosets.getPhoto
 xhr.open('GET', url, true);
 xhr.send(null);
 var photo;
+var i;
+var src;
+var date;
 
 xhr.onreadystatechange = function() {
 	if (xhr.readyState == 4 && xhr.status == 200) {
 		var rsp = xhr.responseXML;
 		photo = rsp.getElementsByTagName('photo');
-		var src = photo[photo.length - 1].attributes[8].value;
-		var date = photo[photo.length - 1].attributes[6].value.split(' ')[0];
-		document.getElementById('l_img').src = src;
-		document.getElementById('d_str').textContent = date;
+		i = photo.length - 1;
+		setPhoto(i);
 	}
 }
+
+function setPhoto(i) {
+	src = photo[i].attributes[8].value;
+	date = photo[i].attributes[6].value.split(' ')[0];
+	document.getElementById('l_img').src = src;
+	document.getElementById('d_str').textContent = date;
+	document.getElementById('d_area').style.visibility = "visible";
+
+	if (i == 0) {
+		document.getElementById('l_lt').style.visibility = "hidden";
+	} else {
+		document.getElementById('l_lt').style.visibility = "visible";
+	}
+	
+	if (i == photo.length - 1) {
+		document.getElementById('l_gt').style.visibility = "hidden";
+	} else {
+		document.getElementById('l_gt').style.visibility = "visible";
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	var lt = document.getElementById('l_lt');
+	lt.addEventListener('click', function() {
+		setPhoto(i - 1);
+		i = i - 1;
+	});
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+	var gt = document.getElementById('l_gt');
+	gt.addEventListener('click', function() {
+		setPhoto(i + 1);
+		i = i + 1;
+	});
+});
